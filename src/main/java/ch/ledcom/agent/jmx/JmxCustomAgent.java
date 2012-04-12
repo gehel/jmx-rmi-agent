9554930bfc366f70aff136399e07a9b443f18e76
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2012-04-12 22:34:04 +0200

    Initial import

---
+++ src/main/java/ch/ledcom/agent/jmx/JmxCustomAgent.java
@@ -0,0 +1,367 @@
+/**
+ *
+ *                                  Apache License
+ *                            Version 2.0, January 2004
+ *                         http://www.apache.org/licenses/
+ *
+ *    TERMS AND CONDITIONS FOR USE, REPRODUCTION, AND DISTRIBUTION
+ *
+ *    1. Definitions.
+ *
+ *       "License" shall mean the terms and conditions for use, reproduction,
+ *       and distribution as defined by Sections 1 through 9 of this document.
+ *
+ *       "Licensor" shall mean the copyright owner or entity authorized by
+ *       the copyright owner that is granting the License.
+ *
+ *       "Legal Entity" shall mean the union of the acting entity and all
+ *       other entities that control, are controlled by, or are under common
+ *       control with that entity. For the purposes of this definition,
+ *       "control" means (i) the power, direct or indirect, to cause the
+ *       direction or management of such entity, whether by contract or
+ *       otherwise, or (ii) ownership of fifty percent (50%) or more of the
+ *       outstanding shares, or (iii) beneficial ownership of such entity.
+ *
+ *       "You" (or "Your") shall mean an individual or Legal Entity
+ *       exercising permissions granted by this License.
+ *
+ *       "Source" form shall mean the preferred form for making modifications,
+ *       including but not limited to software source code, documentation
+ *       source, and configuration files.
+ *
+ *       "Object" form shall mean any form resulting from mechanical
+ *       transformation or translation of a Source form, including but
+ *       not limited to compiled object code, generated documentation,
+ *       and conversions to other media types.
+ *
+ *       "Work" shall mean the work of authorship, whether in Source or
+ *       Object form, made available under the License, as indicated by a
+ *       copyright notice that is included in or attached to the work
+ *       (an example is provided in the Appendix below).
+ *
+ *       "Derivative Works" shall mean any work, whether in Source or Object
+ *       form, that is based on (or derived from) the Work and for which the
+ *       editorial revisions, annotations, elaborations, or other modifications
+ *       represent, as a whole, an original work of authorship. For the purposes
+ *       of this License, Derivative Works shall not include works that remain
+ *       separable from, or merely link (or bind by name) to the interfaces of,
+ *       the Work and Derivative Works thereof.
+ *
+ *       "Contribution" shall mean any work of authorship, including
+ *       the original version of the Work and any modifications or additions
+ *       to that Work or Derivative Works thereof, that is intentionally
+ *       submitted to Licensor for inclusion in the Work by the copyright owner
+ *       or by an individual or Legal Entity authorized to submit on behalf of
+ *       the copyright owner. For the purposes of this definition, "submitted"
+ *       means any form of electronic, verbal, or written communication sent
+ *       to the Licensor or its representatives, including but not limited to
+ *       communication on electronic mailing lists, source code control systems,
+ *       and issue tracking systems that are managed by, or on behalf of, the
+ *       Licensor for the purpose of discussing and improving the Work, but
+ *       excluding communication that is conspicuously marked or otherwise
+ *       designated in writing by the copyright owner as "Not a Contribution."
+ *
+ *       "Contributor" shall mean Licensor and any individual or Legal Entity
+ *       on behalf of whom a Contribution has been received by Licensor and
+ *       subsequently incorporated within the Work.
+ *
+ *    2. Grant of Copyright License. Subject to the terms and conditions of
+ *       this License, each Contributor hereby grants to You a perpetual,
+ *       worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+ *       copyright license to reproduce, prepare Derivative Works of,
+ *       publicly display, publicly perform, sublicense, and distribute the
+ *       Work and such Derivative Works in Source or Object form.
+ *
+ *    3. Grant of Patent License. Subject to the terms and conditions of
+ *       this License, each Contributor hereby grants to You a perpetual,
+ *       worldwide, non-exclusive, no-charge, royalty-free, irrevocable
+ *       (except as stated in this section) patent license to make, have made,
+ *       use, offer to sell, sell, import, and otherwise transfer the Work,
+ *       where such license applies only to those patent claims licensable
+ *       by such Contributor that are necessarily infringed by their
+ *       Contribution(s) alone or by combination of their Contribution(s)
+ *       with the Work to which such Contribution(s) was submitted. If You
+ *       institute patent litigation against any entity (including a
+ *       cross-claim or counterclaim in a lawsuit) alleging that the Work
+ *       or a Contribution incorporated within the Work constitutes direct
+ *       or contributory patent infringement, then any patent licenses
+ *       granted to You under this License for that Work shall terminate
+ *       as of the date such litigation is filed.
+ *
+ *    4. Redistribution. You may reproduce and distribute copies of the
+ *       Work or Derivative Works thereof in any medium, with or without
+ *       modifications, and in Source or Object form, provided that You
+ *       meet the following conditions:
+ *
+ *       (a) You must give any other recipients of the Work or
+ *           Derivative Works a copy of this License; and
+ *
+ *       (b) You must cause any modified files to carry prominent notices
+ *           stating that You changed the files; and
+ *
+ *       (c) You must retain, in the Source form of any Derivative Works
+ *           that You distribute, all copyright, patent, trademark, and
+ *           attribution notices from the Source form of the Work,
+ *           excluding those notices that do not pertain to any part of
+ *           the Derivative Works; and
+ *
+ *       (d) If the Work includes a "NOTICE" text file as part of its
+ *           distribution, then any Derivative Works that You distribute must
+ *           include a readable copy of the attribution notices contained
+ *           within such NOTICE file, excluding those notices that do not
+ *           pertain to any part of the Derivative Works, in at least one
+ *           of the following places: within a NOTICE text file distributed
+ *           as part of the Derivative Works; within the Source form or
+ *           documentation, if provided along with the Derivative Works; or,
+ *           within a display generated by the Derivative Works, if and
+ *           wherever such third-party notices normally appear. The contents
+ *           of the NOTICE file are for informational purposes only and
+ *           do not modify the License. You may add Your own attribution
+ *           notices within Derivative Works that You distribute, alongside
+ *           or as an addendum to the NOTICE text from the Work, provided
+ *           that such additional attribution notices cannot be construed
+ *           as modifying the License.
+ *
+ *       You may add Your own copyright statement to Your modifications and
+ *       may provide additional or different license terms and conditions
+ *       for use, reproduction, or distribution of Your modifications, or
+ *       for any such Derivative Works as a whole, provided Your use,
+ *       reproduction, and distribution of the Work otherwise complies with
+ *       the conditions stated in this License.
+ *
+ *    5. Submission of Contributions. Unless You explicitly state otherwise,
+ *       any Contribution intentionally submitted for inclusion in the Work
+ *       by You to the Licensor shall be under the terms and conditions of
+ *       this License, without any additional terms or conditions.
+ *       Notwithstanding the above, nothing herein shall supersede or modify
+ *       the terms of any separate license agreement you may have executed
+ *       with Licensor regarding such Contributions.
+ *
+ *    6. Trademarks. This License does not grant permission to use the trade
+ *       names, trademarks, service marks, or product names of the Licensor,
+ *       except as required for reasonable and customary use in describing the
+ *       origin of the Work and reproducing the content of the NOTICE file.
+ *
+ *    7. Disclaimer of Warranty. Unless required by applicable law or
+ *       agreed to in writing, Licensor provides the Work (and each
+ *       Contributor provides its Contributions) on an "AS IS" BASIS,
+ *       WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *       implied, including, without limitation, any warranties or conditions
+ *       of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A
+ *       PARTICULAR PURPOSE. You are solely responsible for determining the
+ *       appropriateness of using or redistributing the Work and assume any
+ *       risks associated with Your exercise of permissions under this License.
+ *
+ *    8. Limitation of Liability. In no event and under no legal theory,
+ *       whether in tort (including negligence), contract, or otherwise,
+ *       unless required by applicable law (such as deliberate and grossly
+ *       negligent acts) or agreed to in writing, shall any Contributor be
+ *       liable to You for damages, including any direct, indirect, special,
+ *       incidental, or consequential damages of any character arising as a
+ *       result of this License or out of the use or inability to use the
+ *       Work (including but not limited to damages for loss of goodwill,
+ *       work stoppage, computer failure or malfunction, or any and all
+ *       other commercial damages or losses), even if such Contributor
+ *       has been advised of the possibility of such damages.
+ *
+ *    9. Accepting Warranty or Additional Liability. While redistributing
+ *       the Work or Derivative Works thereof, You may choose to offer,
+ *       and charge a fee for, acceptance of support, warranty, indemnity,
+ *       or other liability obligations and/or rights consistent with this
+ *       License. However, in accepting such obligations, You may act only
+ *       on Your own behalf and on Your sole responsibility, not on behalf
+ *       of any other Contributor, and only if You agree to indemnify,
+ *       defend, and hold each Contributor harmless for any liability
+ *       incurred by, or claims asserted against, such Contributor by reason
+ *       of your accepting any such warranty or additional liability.
+ *
+ *    END OF TERMS AND CONDITIONS
+ *
+ *    APPENDIX: How to apply the Apache License to your work.
+ *
+ *       To apply the Apache License to your work, attach the following
+ *       boilerplate notice, with the fields enclosed by brackets "[]"
+ *       replaced with your own identifying information. (Don't include
+ *       the brackets!)  The text should be enclosed in the appropriate
+ *       comment syntax for the file format. We also recommend that a
+ *       file or class name and description of purpose be included on the
+ *       same "printed page" as the copyright notice for easier
+ *       identification within third-party archives.
+ *
+ *    Copyright 2012 LedCom
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package ch.ledcom.agent.jmx;
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.rmi.registry.LocateRegistry;
+import java.util.HashMap;
+import javax.management.MBeanServer;
+import javax.management.remote.JMXConnectorServer;
+import javax.management.remote.JMXConnectorServerFactory;
+import javax.management.remote.JMXServiceURL;
+
+/**
+ * This CustomAgent will start an RMI Connector Server using only port
+ * "ch.ledcom.agent.jmx.port". When used with the javaagent option, it
+ * allows a remote connection using JConsole or VisualVM through an SSH tunnel.
+ * 
+ * The purpose of this agent is to open a JMX server that will use only one
+ * fixed port for all communication instead of two dynamically set ports.
+ * 
+ * Code adapted from:
+ * http://blogs.sun.com/jmxetc/entry/connecting_through_firewall_using_jmx
+ * 
+ * @author gehel
+ */
+public class JmxCustomAgent {
+
+	private static final String BASE = "ch.ledcom.agent.jmx.";
+	private static final String PORT_KEY = BASE + "port";
+	private static final String FORCE_LOCALHOST_KEY = BASE + "forceLocalhost";
+	private static final String PASSWORD_FILE = BASE + "password.file";
+	private static final String ACCESS_FILE = BASE + "access.file";
+
+	private JmxCustomAgent() {
+	}
+
+	public static void premain(String agentArgs) throws IOException {
+
+		String portStr = System.getProperty(PORT_KEY);
+		if ((portStr == null) || portStr.isEmpty()) {
+			System.err.println();
+			System.err.println();
+			System.err
+					.println("******************************************************");
+			System.err.println("*");
+			System.err.println("* RMI / JMX Custom Agent");
+			System.out.println("* ======================");
+			System.err.println("*");
+			System.err.println("* !!! WARNING !!!");
+			System.err.println("*");
+			System.err
+					.println("* RMI/JMX port that must be defined with system property: -D"
+							+ PORT_KEY
+							+ " is missing  ==>  no RMI/JMX server started");
+			System.err.println("*");
+			System.err
+					.println("* Please configure the port correctly to remove this warning.");
+			System.err.println("*");
+			System.err
+					.println("******************************************************");
+			System.err.println();
+			System.err.println();
+			return;
+		}
+
+		System.out
+				.println("******************************************************");
+		System.out.println("*");
+		System.out.println("* RMI / JMX Custom Agent");
+		System.out.println("* ======================");
+		System.out.println("*");
+
+		// Ensure cryptographically strong random number generator used
+		// to choose the object number - see java.rmi.server.ObjID
+		//
+		System.setProperty("java.rmi.server.randomIDs", "true");
+
+		// Start an RMI registry on port specified by example.rmi.agent.port
+		// (default 3000).
+		//
+		final int port = Integer.parseInt(portStr);
+		System.out.println("* Create RMI registry on port " + port);
+		LocateRegistry.createRegistry(port);
+
+		// Retrieve the PlatformMBeanServer.
+		//
+		System.out.println("* Get the platform's MBean server");
+		MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
+
+		// Environment map.
+		//
+		System.out.println("* Initialize the environment map");
+		HashMap<String, Object> env = new HashMap<String, Object>();
+
+		// Provide the password file used by the connector server to
+		// perform user authentication. The password file is a properties
+		// based text file specifying username/password pairs.
+
+		System.out.println("jmx.remote.x.password.file="
+				+ System.getProperty(PASSWORD_FILE));
+		env.put("jmx.remote.x.password.file", System.getProperty(PASSWORD_FILE));
+
+		// Provide the access level file used by the connector server to
+		// perform user authorization. The access level file is a properties
+		// based text file specifying username/access level pairs where
+		// access level is either "readonly" or "readwrite" access to the
+		// MBeanServer operations.
+
+		System.out.println("jmx.remote.x.access.file="
+				+ System.getProperty(ACCESS_FILE));
+		env.put("jmx.remote.x.access.file", System.getProperty(ACCESS_FILE));
+		env.put("jmx.remote.x.authenticate", Boolean.TRUE);
+
+		// Create an RMI connector server.
+		//
+		// As specified in the JMXServiceURL the RMIServer stub will be
+		// registered in the RMI registry running in the local host on
+		// port 3000 with the name "jmxrmi". This is the same name the
+		// out-of-the-box management agent uses to register the RMIServer
+		// stub too.
+		//
+		// The port specified in "service:jmx:rmi://"+hostname+":"+port
+		// is the second port, where RMI connection objects will be exported.
+		// Here we use the same port as that we choose for the RMI registry.
+		// The port for the RMI registry is specified in the second part
+		// of the URL, in "rmi://"+hostname+":"+port
+		//
+		boolean forceLocalhost = Boolean.getBoolean(FORCE_LOCALHOST_KEY);
+		String hostname;
+		if (forceLocalhost) {
+			System.out
+					.println("* Using localhost as hostname as defined by system property: "
+							+ FORCE_LOCALHOST_KEY);
+			hostname = "localhost";
+		} else {
+			hostname = InetAddress.getLocalHost().getHostName();
+		}
+
+		System.out
+				.println("* Create an RMI/JMX connector server, hostname is: "
+						+ hostname);
+		String serviceUrl = "service:jmx:rmi://" + hostname + ":" + port
+				+ "/jndi/rmi://" + hostname + ":" + port + "/jmxrmi";
+		System.out.println("*  and url is: " + serviceUrl);
+		JMXServiceURL url = new JMXServiceURL(serviceUrl);
+
+		// Now create the server from the JMXServiceURL
+		//
+		JMXConnectorServer cs = JMXConnectorServerFactory
+				.newJMXConnectorServer(url, env, mbs);
+
+		// Start the RMI connector server.
+		//
+		System.out.println("* Start the RMI/JMX connector server on port "
+				+ port);
+		cs.start();
+
+		System.out.println("*");
+		System.out
+				.println("******************************************************");
+		System.out.println();
+	}
+}
