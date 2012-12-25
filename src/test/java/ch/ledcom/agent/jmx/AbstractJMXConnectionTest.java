b3368dfd028421e696cc365f1fcc345a0db6b42c
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2012-12-26 00:15:51 +0100

    Tests are not dependent on the project version anymore

--- src/test/java/ch/ledcom/agent/jmx/AbstractJMXConnectionTest.java
+++ src/test/java/ch/ledcom/agent/jmx/AbstractJMXConnectionTest.java
@@ -208,67 +208,76 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
 import org.junit.After;
 
 import com.google.common.io.ByteStreams;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+
 public abstract class AbstractJMXConnectionTest {
 
-	private static final int PAUSE = 500;
-	
-	private Process process;
-	
-	public void startDummyApplication(String... additionalArguments) throws IOException,
-			InterruptedException {
-		List<String> args = new ArrayList<String>();
-		args.add("java");
-		args.add("-javaagent:target/jmx-rmi-agent-0.1-SNAPSHOT.jar");
-		args.add("-cp");
-		args.add("target/test-classes/");
-		
-		args.addAll(Arrays.asList(additionalArguments));
-		
-		args.add(DummyApp.class.getName());
-		ProcessBuilder pBuilder = new ProcessBuilder(args);
-		pBuilder.redirectErrorStream(true);
+    private static final int PAUSE = 500;
 
-		process = pBuilder.start();
-		redirectProcessIO(process);
-		Thread.sleep(PAUSE);
-	}
+    private Process process;
 
-	@After
-	public void stopDummyApplication() throws InterruptedException {
-		process.destroy();
-		Thread.sleep(PAUSE);
-	}
+    public void startDummyApplication(String... additionalArguments) throws IOException,
+            InterruptedException {
+        List<String> args = new ArrayList<String>();
+        args.add("java");
+        args.add("-javaagent:target/jmx-rmi-agent-" + getVersion() + ".jar");
+        args.add("-cp");
+        args.add("target/test-classes/");
 
-	private void redirectProcessIO(final Process process) {
-		new Thread(new Runnable() {
-			public void run() {
-				try {
-					ByteStreams.copy(process.getInputStream(), System.out);
-				} catch (IOException e) {
-				}
-			}
-		}).start();
-		new Thread(new Runnable() {
-			public void run() {
-				try {
-					ByteStreams.copy(process.getErrorStream(), System.err);
-				} catch (IOException e) {
-				}
-			}
-		}).start();
-		new Thread(new Runnable() {
-			public void run() {
-				try {
-					ByteStreams.copy(System.in, process.getOutputStream());
-				} catch (IOException e) {
-				}
-			}
-		}).start();
-	}
+        args.addAll(Arrays.asList(additionalArguments));
 
+        args.add(DummyApp.class.getName());
+        ProcessBuilder pBuilder = new ProcessBuilder(args);
+        pBuilder.redirectErrorStream(true);
+
+        process = pBuilder.start();
+        redirectProcessIO(process);
+        Thread.sleep(PAUSE);
+    }
+
+    @After
+    public void stopDummyApplication() throws InterruptedException {
+        process.destroy();
+        Thread.sleep(PAUSE);
+    }
+
+    private void redirectProcessIO(final Process process) {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    ByteStreams.copy(process.getInputStream(), System.out);
+                } catch (IOException e) {
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    ByteStreams.copy(process.getErrorStream(), System.err);
+                } catch (IOException e) {
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    ByteStreams.copy(System.in, process.getOutputStream());
+                } catch (IOException e) {
+                }
+            }
+        }).start();
+    }
+
+    private String getVersion() throws IOException {
+        Properties props = new Properties();
+        props.load(
+                AbstractJMXConnectionTest.class.getClassLoader().getResourceAsStream("jmx-rmi-agent.properties"));
+        return props.getProperty("jmx-rmi-agent.version");
+    }
 }
